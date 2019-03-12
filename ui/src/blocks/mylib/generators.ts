3b7cb7522a0e26c8d3d192b2659620b1763bdd98
mc03225 <45171925+mc03225@users.noreply.github.com>
2019-03-12 16:36:22 +0000

    Update generators.ts

--- ui/src/blocks/mylib/generators.ts
+++ ui/src/blocks/mylib/generators.ts
@@ -1,5 +1,9 @@
 export default function define(Python: Blockly.BlockGenerators) {
- 
+Python['import_mylib'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  var code = 'import mylib\n';
+  return code;
+}; 
  
 }
 
