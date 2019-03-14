b37394661c0392f1db215a88c31f25688b77de03
mc03225 <45171925+mc03225@users.noreply.github.com>
2019-03-14 18:50:11 +0000

    Update definitions.ts

--- ui/src/blocks/mylib/definitions.ts
+++ ui/src/blocks/mylib/definitions.ts
@@ -1,29 +1,29 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
-Blockly.Blocks['import_mylib'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("import mylib");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(135);
- this.setTooltip("Imports mylib");
- this.setHelpUrl("");
-  }
-}; 
+  Blockly.Blocks['import_mylib'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("import mylib");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(135);
+   this.setTooltip("Imports mylib");
+   this.setHelpUrl("");
+    }
+  }; 
   
-Blockly.Blocks['mylib_cmd'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("mylib.")
-        .appendField(new Blockly.FieldTextInput("hello"), "input")
-        .appendField("()");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(135);
- this.setTooltip("Command for mylib");
- this.setHelpUrl("");
-  }
-};
+  Blockly.Blocks['mylib_cmd'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("mylib.")
+          .appendField(new Blockly.FieldTextInput("hello"), "input")
+          .appendField("()");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(135);
+   this.setTooltip("Command for mylib");
+   this.setHelpUrl("");
+    }
+  };
  
 }
 
