1cef46402e9bb161219f17275c536e45f2028eba
mc03225 <45171925+mc03225@users.noreply.github.com>
2019-03-15 12:43:21 +0000

    Update definitions.ts

--- ui/src/blocks/mylib/definitions.ts
+++ ui/src/blocks/mylib/definitions.ts
@@ -1,5 +1,5 @@
 export default function define(Blocks: Blockly.BlockDefinitions) {
-  Blockly.Blocks['import_mylib'] = {
+  Blocks['import_mylib'] = {
     init: function() {
       this.appendDummyInput()
           .appendField("import mylib");
@@ -11,7 +11,7 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
     }
   }; 
   
-  Blockly.Blocks['mylib_cmd'] = {
+  Blocks['mylib_cmd'] = {
     init: function() {
       this.appendDummyInput()
           .appendField("mylib.")
